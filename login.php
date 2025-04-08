6020d38df840a4272efece027354f6ae52a5d86b
Gabriel Lemos <gabriel97rl@gmail.com>
2025-04-08 13:45:34 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+require 'connection.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nome_professor = $_POST['nome_professor'];
+        $senha = $_POST['senha'];
+        $disciplina = $_POST['disciplina'];
+    
+        $stmt = $conn->prepare("INSERT INTO professores (nome_professor, senha, id_disciplina) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $nome_professor, $senha, $disciplina);
+    
+        if ($stmt->execute()) {
+            echo "<script>alert('Login realizado com sucesso!'); window.location.href='login.html';</script>";
+        }        
+        else {
+            echo "<script>alert('Erro ao realizar login! (erro na conexão).'); window.location.href='login.html';</script>";
+        }
+    }
+    $stmt->close();
+    $conn->close();
+    exit();
+?>
