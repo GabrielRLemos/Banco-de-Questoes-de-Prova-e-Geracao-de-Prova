ba83bdbf8b7a256a7256efc061878ff30298d5af
FranciscoMaycon <mayconlimapamela@gmail.com>
2025-04-10 15:40:12 -0300

    Update cadastrar.php

--- Actions/cadastrar.php
+++ Actions/cadastrar.php
@@ -2,18 +2,18 @@
 require 'connection.php';
 
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        $pergunta = $_POST['pergunta'];
+        $nome_professor = $_POST['nome_professor'];
+        $senha = $_POST['senha'];
         $disciplina = $_POST['disciplina'];
-        $assunto = $_POST['assunto'];
     
-        $stmt = $conn->prepare("INSERT INTO questoes (pergunta, id_disciplina) VALUES (?, ?, ?)");
-        $stmt->bind_param("ssi", $nome_, $disciplina);
+        $stmt = $conn->prepare("INSERT INTO professores (nome_professor, senha, id_disciplina) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $nome_professor, $senha, $disciplina);
     
         if ($stmt->execute()) {
-            echo "<script>alert('Login realizado com sucesso!'); window.location.href='cadastrar.html';</script>";
+            echo "<script>alert('Cadastro realizado com sucesso!'); window.location.href='cadastrar.html';</script>";
         }        
         else {
-            echo "<script>alert('Erro ao realizar login! (erro na conexão).'); window.location.href='cadastrar.html';</script>";
+            echo "<script>alert('Erro ao realizar cadastro! (erro na conexão).'); window.location.href='cadastrar.html';</script>";
         }
     }
     $stmt->close();
